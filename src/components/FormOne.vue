3060a6499c4aa17f29010e5f74452c01837bee92
rmrokon <mdrokon7773@gmail.com>
2022-11-26 01:34:18 +0600

    input and checkbox binding

--- src/components/FormOne.vue
+++ src/components/FormOne.vue
@@ -1,9 +1,31 @@
 <template>
-    <form-helper>
-        <div slot="form-fields">
-            <textarea name="" placeholder="Ented text"></textarea>
+    <div>
+        <form-helper>
+            <div slot="form-fields">
+                <input type="text" v-model.lazy="blog.title">
+                <textarea name="" placeholder="Ented text" v-model.lazy="blog.content"></textarea>
+
+                <div id="checkboxes">
+                    <label>Cat 1</label>
+                    <input type="checkbox" value="cat 1" v-model="blog.cats">
+                    <label>Cat 2</label>
+                    <input type="checkbox" value="cat 2" v-model="blog.cats">
+                    <label>Cat 3</label>
+                    <input type="checkbox" value="cat 3" v-model="blog.cats">
+                </div>
+            </div>
+        </form-helper>
+
+        <div id="preview">
+            <h3>Title: {{ blog.title }}</h3>
+            <p>Description:</p>
+            <p>{{ blog.content }}</p>
+            <p>Cats:</p>
+            <ul>
+                <li v-for="cat in blog.cats">{{ cat }}</li>
+            </ul>
         </div>
-    </form-helper>
+    </div>
 </template>
 
 <script>
@@ -11,6 +33,16 @@ import FormHelper from './FormHelper.vue'
 export default {
     components: {
         'form-helper': FormHelper
+    },
+
+    data() {
+        return {
+            blog: {
+                title: '',
+                content: '',
+                cats: []
+            }
+        }
     }
 }
 </script>
